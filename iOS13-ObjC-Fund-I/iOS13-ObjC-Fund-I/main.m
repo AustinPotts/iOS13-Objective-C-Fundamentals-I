22f88d712a89a11385686a0ad111fac895cd07be
AustinPotts <pottsevolvements@gmail.com>
2020-03-16 12:24:14 -0400

    Configred String Example

--- iOS13-ObjC-Fund-I/iOS13-ObjC-Fund-I/main.m
+++ iOS13-ObjC-Fund-I/iOS13-ObjC-Fund-I/main.m
@@ -6,12 +6,23 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h> // Used for frameworks 
+#import "LSILog.h" // used for project files
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
+    @autoreleasepool { // automatic Reference Counting  (ARC) - auto release objects
+        
+        
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        
+       // let honda = "Civic" //Swift
+       // printf("I have a \(honda)") // Swift
+        
+        NSString *honda = @"Civic";  //Objective C is based on C
+        NSLog(@"I have a %@ from %d", honda, 2010); // Using a string token to print (%@ = Object)
+        //%d
     }
     return 0;
 }

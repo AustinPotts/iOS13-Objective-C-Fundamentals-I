94ba419c3f097d172a82f1aa0fa70bf8c99f0229
AustinPotts <pottsevolvements@gmail.com>
2020-03-16 12:13:03 -0400

    Configured

---
+++ iOS13-ObjC-Fund-I/iOS13-ObjC-Fund-I/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS13-ObjC-Fund-I
+//
+//  Created by Austin Potts on 3/16/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
